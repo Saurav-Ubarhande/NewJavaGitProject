92a6bb72043cc9970a2579fc8efb092e29410905
Saurav-Ubarhande <1507saurav@gmail.com>
2021-07-28 15:09:54 +0530

    third commit

--- NewJavaGitProject/src/main/java/com/lti/demo/MyFile.java
+++ NewJavaGitProject/src/main/java/com/lti/demo/MyFile.java
@@ -5,6 +5,7 @@ public class MyFile {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("MyFile commit");
+		System.out.println("Updated");
 	
 	}
 }
