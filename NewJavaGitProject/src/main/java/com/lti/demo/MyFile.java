43657ae6a511d4e319afd0aed70e736f2a936c98
Saurav-Ubarhande <46933684+Saurav-Ubarhande@users.noreply.github.com>
2021-07-28 15:06:53 +0530

    Create MyFile.java

---
+++ NewJavaGitProject/src/main/java/com/lti/demo/MyFile.java
@@ -0,0 +1,10 @@
+package com.lti.demo;
+
+public class MyFile {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+		System.out.println("MyFile commit");
+	
+	}
+}
